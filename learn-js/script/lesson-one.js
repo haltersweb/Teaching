90a6a2eb7eb447cce12860991b862dfe5f48b3c5
Adina Halter <haltersweb@gmail.com>
2016-08-20 14:26:47 -0400

    hide and show switch buttons

--- learn-js/script/lesson-one.js
+++ learn-js/script/lesson-one.js
@@ -6,9 +6,13 @@ var screen = document.getElementsByTagName("BODY")[0];
 switchOn.addEventListener("click", function () {
 	bulb.src = "images/pic_bulbon.gif";
 	screen.className = "night-time";
+	switchOn.style.display = "none";
+	switchOff.style.display = "inline-block";
 });
 
 switchOff.addEventListener("click", function () {
 	bulb.src = "images/pic_bulboff.gif";
 	screen.className = "";
+	switchOn.style.display = "inline-block";
+	switchOff.style.display = "none";
 });
